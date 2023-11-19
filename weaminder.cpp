2aebe06dc0abe3eee55b7c92ab670227a6432dd2
Ngo Phu Hien <ngophuhien029@gmail.com>
2023-11-19 10:13:44 +0700

    Update check connection on/off function
    The previous code I copied from ChatGPT

--- weaminder.cpp
+++ weaminder.cpp
@@ -9,13 +9,6 @@ Weaminder::Weaminder(QObject *parent)
 bool Weaminder::isWifiOn()
 {
     QNetworkConfigurationManager manager;
-    QList<QNetworkConfiguration> configs = manager.allConfigurations(QNetworkConfiguration::Active);
 
-    foreach (const QNetworkConfiguration &config, configs) {
-        if (config.bearerType() == QNetworkConfiguration::BearerWLAN) {
-            return true;
-        }
-    }
-
-    return false;
+    return manager.isOnline();
 }
